7f2caa5325e262fb32f3a8301d7e629c7b06376a
Berserk-21 <cryptogekkofr@protonmail.com>
2024-05-07 10:19:49 +0200

    Build the layout basis with Text views, DatePicker and Stepper

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -10,11 +10,35 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        VStack {
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted())", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                Stepper("\(coffeeAmount)", value: $coffeeAmount, in: 1...20, step: 1)
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate") {
+                    calculateBetTime()
+                }
+            }
         }
+    }
+    
+    private func calculateBetTime() {
+        
     }
 }
 
